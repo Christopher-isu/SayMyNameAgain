87f195fd2684aad0e13d9546aed99918c3a37ef7
chris <chris@DESKTOP-NTTCC6T>
2025-02-02 22:32:23 -0700

    final

---
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -0,0 +1,35 @@
+﻿'ChristopherZ
+'Spring 2025
+'RCET2265
+'Operators Example
+'https://github.com/Christopher-isu/SayMyNameAgain.git
+
+Option Explicit On 'forces all variables to be declared
+Option Strict On 'forces all variables to be declared correctly
+Option Compare Text 'makes the code case insensitive
+Module SayMyNameAgain
+    Sub Main()
+        ' Prompt the user to enter their name
+        Console.Write("Enter your name: ")
+
+        ' Save user input as a variable
+        Dim userName As String = Console.ReadLine()
+
+        Select Case userName
+            Case "Emily"
+                Console.WriteLine("Hello " & (userName))
+            Case "Joe"
+                Console.WriteLine("Hello " & (userName))
+            Case "Christopher"
+                Console.WriteLine("There can be only one " & (userName) & " !")
+            Case Else
+                Console.WriteLine("Your name is not on the list.")
+        End Select
+
+        ' Keep console open untill button pressed
+        Console.WriteLine("Press any key to exit.")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
